86e046cf63460cc7dd36b87196a1fc3d45b00c33
Jeff Pittman <geojeff@jeff-pittmans-macbook-pro.local>
2011-12-14 10:36:32 -0600

    Added several more command line parameter tests.

--- tests/command_line_parameters.js
+++ tests/command_line_parameters.js
@@ -13,13 +13,29 @@ vows.describe('command line arguments').addBatch({                    // Batch
       },
     'containing the input filename and the targets parameter': {            // Sub-Context
       topic: ['node', 'linelizer.js', 'CHANGELOG-SC.md', '--targets=blue'], // Topic
-        'should return two parameters': function (topic) {                  // Vow
+      'should return two parameters': function (topic) {                    // Vow
+        var result = linelizer.gatherCommandLineParameters(topic);
+        assert.equal(result.input, 'CHANGELOG-SC.md');
+        assert.equal(result.targets, 'blue');
+      }
+    },
+    'containing the targets parameter as "statecharts and routes, testing"': {
+      topic: ['node', 'linelizer.js', 'CHANGELOG-SC.md', '--targets=statecharts and routes, testing'], // Odd quoting needed.
+        'should return two parameters': function (topic) {
           var result = linelizer.gatherCommandLineParameters(topic);
           assert.equal(result.input, 'CHANGELOG-SC.md');
-          assert.equal(result.targets, 'blue');
+          assert.equal(result.targets, 'statecharts and routes, testing');
+        }
+      },
+    'containing the input filename and the search parameter': {
+      topic: ['node', 'linelizer.js', 'CHANGELOG-SC.md', '--search=exhaustive'],
+        'should return two parameters': function (topic) {
+          var result = linelizer.gatherCommandLineParameters(topic);
+          assert.equal(result.input, 'CHANGELOG-SC.md');
+          assert.equal(result.search, 'exhaustive');
         }
       }
-    }
+  }
 }).export(module);
 
 
