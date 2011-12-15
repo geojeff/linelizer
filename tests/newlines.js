5a60ed1643f197344746c36ed500f7769aca93ca
Jeff Pittman <geojeff@jeff-pittmans-macbook-pro.local>
2011-12-15 10:21:35 -0600

    Improved layout and flow of unwanted lines tests.

--- tests/newlines.js
+++ tests/newlines.js
@@ -7,15 +7,15 @@ vows.describe('newlines').addBatch({                                  // Batch
         'beginning with *, and broken with unwanted newlines': {      // Sub-Context
             topic: ["* one\n", "  two\n", "  three\n"],               // Topic
             'should be joined, to length 1': function (topic) {       // Vow
-              var fixedTopic = linelizer.fixNewlines(topic);
-              assert.equal(fixedTopic.length, 1);
+              var fixedLines = linelizer.fixNewlines(topic);
+              assert.equal(fixedLines.length, 1);
             }
         },
         'beginning with -, and broken with unwanted newlines': {      // Sub-Context
             topic: ["- one\n", "  two\n", "  three\n"],               // Topic
             'should be joined, to length 1': function (topic) {       // Vow
-              var fixedTopic = linelizer.fixNewlines(topic);
-              assert.equal(fixedTopic.length, 1);
+              var fixedLines = linelizer.fixNewlines(topic);
+              assert.equal(fixedLines.length, 1);
             }
         }
     }
--- tests/unwanted_lines.js
+++ tests/unwanted_lines.js
@@ -2,22 +2,27 @@ var vows = require('vows'),
     assert = require('assert'),
     linelizer = require('../linelizer');
 
-vows.describe('unwanted lines').addBatch({                                         // Batch
-  'A set of input lines': {                                                        // Context
-    'containing * blah, - blah, -------, =======, 1.6.1, cat, and // dog': {       // Sub-Context
-      topic: ['* blah', '- blah', '------', '======', '1.6.1', 'cat', '// dog'],   // Topic
-        'should be reduced to three lines': function (topic) {                     // Vow
-          var lines = linelizer.removeUnwantedLines(topic);
-          assert.equal(lines.length, 3);
-        },
-        'should be reduced to * blah, -blah, and 1.6.1': function (topic) {        // Vow
-          var lines = linelizer.removeUnwantedLines(topic);
-          assert.include(lines, '* blah');
-          assert.include(lines, '- blah');
-          assert.include(lines, '1.6.1');
-        }
-      }
+vows.describe('unwanted lines').addBatch({
+  'Calling removeUnwantedLines on: * blah, - blah, -------, =======, 1.6.1, cat, and // dog': {
+    topic: linelizer.removeUnwantedLines(['* blah',
+                                          '- blah',
+                                          '------',
+                                          '======',
+                                          '1.6.1',
+                                          'cat',
+                                          '// dog']),
+    'should return an array': function (topic) {
+      assert.instanceOf(topic, Array);
+    },
+    'with three lines': function (topic) {
+      assert.equal(topic.length, 3);
+    },
+    'containing * blah, -blah, and 1.6.1': function (topic) {
+      assert.include(topic, '* blah');
+      assert.include(topic, '- blah');
+      assert.include(topic, '1.6.1');
     }
+  }
 }).export(module);
 
 
